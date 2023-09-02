7527527b8fb49fb56c38481c3eec75920cd969c8
Zahromedes <142671917+Zahromedes@users.noreply.github.com>
2023-09-02 20:02:11 +0300

    Create cw4.py

---
+++ cw4.py
@@ -0,0 +1,41 @@
+def My_name():
+    print(f"My name is Zahraa ")
+My_name()
+
+def My_meal(food , Drink ):
+    print(f"I like to eat {food}, while drinking {Drink}. ")
+
+
+My_meal("sushi" , "w")
+
+
+def cube(number):
+   
+   return number**3
+   
+
+result = cube(3)
+print(result)
+
+def By_three(num):
+    if num%3==0:
+        result = cube(num)
+        return result
+    else :
+        return False
+
+print(By_three(5))
+    
+
+
+
+
+
+
+
+# def By_three(number):
+#     if number%3==0
+#     True 
+# else:
+# False
+    
